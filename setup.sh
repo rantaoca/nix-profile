7bc43b52e7e7daf607a2a9fdccc7ceec2b4e3510
Ran Tao <rantaoca@google.com>
2018-11-10 10:48:19 -0800

    Add pathogen vim plugin manager to setup.sh

--- setup.sh
+++ setup.sh
@@ -13,6 +13,10 @@ printf "\nsource-file ~/.ranconf/.tmux.conf\n" >> ~/.tmux.conf
 # Include .vimrc
 printf "\nsource ~/.ranconf/.vimrc\n" >> ~/.vimrc 
 
+# Install Vim Pathon Plugin Manager
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 # Vim Pathogen Plugin Manager Plugins
 git clone git://github.com/tpope/vim-abolish.git ~/.vim/bundle/vim-abolish
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
