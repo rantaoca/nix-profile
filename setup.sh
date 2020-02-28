4a0d6d2a7add0f5b96139fe3c6e8b8a522ef4c64
Ran Tao <rtao@bostondynamics.com>
2020-02-27 16:14:43 -0800

    Adding fzf reverse search to setup.sh

--- setup.sh
+++ setup.sh
@@ -22,6 +22,10 @@ printf "\nsource ~/.ranconf/.vimrc\n" >> ~/.vimrc
 echo Downloading Tmux Plugin Manager...
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+echo Installing fuzzy reverse search...
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
 touch ~/.display.txt
 mkdir ~/.logs
 
